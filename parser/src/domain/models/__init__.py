08260ad6668378d040ba37efc7e80e8af93f44e3
deyna256 <literallybugcreator@gmail.com>
2026-02-09 18:09:43 +0300

    Remove redundant reimport in `domain/models` of `parser`

--- parser/src/domain/models/__init__.py
+++ parser/src/domain/models/__init__.py
@@ -1,16 +1,1 @@
 """Domain models package."""
-
-from .contest import Contest, ContestProblem
-from .identifiers import ContestIdentifier, ProblemIdentifier
-from .parsing import ContestPageData, ProblemData
-from .problem import Problem
-
-__all__ = [
-    "Contest",
-    "ContestIdentifier",
-    "ContestPageData",
-    "ContestProblem",
-    "Problem",
-    "ProblemData",
-    "ProblemIdentifier",
-]
